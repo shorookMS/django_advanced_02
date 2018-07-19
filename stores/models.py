fe2c14549f7946fae4db4f0a7f4a2fbed969d765
Hussein Almutawa <hussein@joincoded.com>
2018-07-19 11:35:57 +0300

    Modified model

--- stores/models.py
+++ stores/models.py
@@ -7,29 +7,9 @@ from django.dispatch import receiver
 class Store(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
-
-def create_slug(instance, new_slug=None):
-    slug = slugify (instance.name)
-    if new_slug is not None:
-        slug = new_slug
-    qs = Store.objects.filter(slug=slug)
-    if qs.exists():
-        if "-" in slug:
-            slug_list = slug.split("-")
-            new_slug = "%s-%s"%(slug_list[0],int(slug_list[1])+1)
-        else:
-            new_slug = "%s-1"%(slug)
-        return create_slug(instance, new_slug=new_slug)
-    return slug
-
-@receiver(pre_save, sender=Store)
-def pre_save_store_receiver(sender, instance, *args, **kwargs):
-    if not instance.slug:
-        instance.slug = create_slug(instance)
-
 
 
